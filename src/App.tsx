c09b63d1983102b6dae0a2716dcd80108df40049
Hanna <sophianhanna@gmail.com>
2021-11-05 14:41:41 +0000

    Added episodes.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { greet } from "./utils/greet";
+import episodes from "./episodes.json"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <>
+    <h1>{greet("World")}</h1>
+    {console.log(episodes)}
+  </>
+  )
 }
 
 export default App;
