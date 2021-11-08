e8bfc92415d187fdd4bda72b534bddf337a3b653
Hanna <sophianhanna@gmail.com>
2021-11-08 15:04:06 +0000

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import episodes from "./episodes.json"
+import episodes from "./episodes.json";
 import EpisodesEntry from "./EpisodesEntry";
 
 function App(): JSX.Element {
-  const allEpisodes = episodes.map((episode) => 
-    <EpisodesEntry key={episode.id}
-      id = {episode.id}
-      url = {episode.url}
-      name = {episode.name}
-      season = {episode.season}
-      number = {episode.number}
-      type = {episode.type}
-      airdate = {episode.airdate}
-      airtime = {episode.airtime}
-      airstamp = {episode.airstamp}
-      runtime = {episode.runtime}
-      image = {{
-        medium : episode.image.medium,
-        original : episode.image.original,
+  const allEpisodes = episodes.map((episode) => (
+    <EpisodesEntry
+      key={episode.id}
+      id={episode.id}
+      url={episode.url}
+      name={episode.name}
+      season={episode.season}
+      number={episode.number}
+      type={episode.type}
+      airdate={episode.airdate}
+      airtime={episode.airtime}
+      airstamp={episode.airstamp}
+      runtime={episode.runtime}
+      image={{
+        medium: episode.image.medium,
+        original: episode.image.original,
       }}
-      summary = {episode.summary}
-      _links = {{self : {href : episode._links.self.href}}}
-  />
-  )
+      summary={episode.summary}
+      _links={{ self: { href: episode._links.self.href } }}
+    />
+  ));
   return (
-  <>
+    <div className="App">
       <h1>TV List</h1>
       {allEpisodes}
-  </>
-  )
+    </div>
+  );
 }
 
 export default App;
--- src/EpisodesEntry.tsx
+++ src/EpisodesEntry.tsx
@@ -1,39 +1,39 @@
 interface IEpisode {
-    id: number;
-    url: string;
-    name: string;
-    season: number;
-    number: number;
-    type: string;
-    airdate: string;
-    airtime: string;
-    airstamp: string;
-    runtime: number;
-    image: {
-      medium: string;
-      original: string;
-    };
-    summary: string;
-    _links: { self: { href: string } };
-  }
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
 
 function EpisodesEntry(props: IEpisode): JSX.Element {
   function removeTags(str: string): string {
-    if ((str===null) || (str===''))
-        return "";
-    else
-        str = str.toString();
-        return str.replace( /(<([^>]+)>)/ig, '');
+    if (str === null || str === "") return "";
+    else str = str.toString();
+    return str.replace(/(<([^>]+)>)/gi, "");
   }
 
   return (
-      <section> 
-          <h3>{props.name} - S{props.season}E{props.number}</h3>
-          <img src={props.image.medium} alt={props.name}/>
-          <br />
-          {removeTags(props.summary)}
-      </section>
-  )
+    <section>
+      <h3>
+        {props.name} - S{props.season}E{props.number}
+      </h3>
+      <img src={props.image.medium} alt={props.name} />
+      <br />
+      {removeTags(props.summary)}
+    </section>
+  );
 }
 
 export default EpisodesEntry;
