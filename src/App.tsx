6cf6675b00e0b1bc826ee2d683595c30b853156a
Hanna <sophianhanna@gmail.com>
2021-11-08 08:41:58 +0000

    Added map to display episodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { greet } from "./utils/greet";
 import episodes from "./episodes.json"
+import EpisodesEntry from "./EpisodesEntry";
 
 function App(): JSX.Element {
+  const allEpisodes = episodes.map((episode) => 
+    <EpisodesEntry key={episode.id}
+      id = {episode.id}
+      url = {episode.url}
+      name = {episode.name}
+      season = {episode.season}
+      number = {episode.number}
+      type = {episode.type}
+      airdate = {episode.airdate}
+      airtime = {episode.airtime}
+      airstamp = {episode.airstamp}
+      runtime = {episode.runtime}
+      image = {{
+        medium : episode.image.medium,
+        original : episode.image.original,
+      }}
+      summary = {episode.summary}
+      _links = {{self : {href : episode._links.self.href}}}
+  />
+  )
   return (
   <>
-    <h1>{greet("World")}</h1>
-    {console.log(episodes)}
+      <h1>TV List</h1>
+      {allEpisodes}
   </>
   )
 }
