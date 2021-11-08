cfc873bb95e88e71d94232d147094721ed737ccb
Hanna <sophianhanna@gmail.com>
2021-11-08 15:20:43 +0000

    Created getEpisodeCode function

---
+++ src/utils/get-episode-code.test.ts
@@ -0,0 +1,10 @@
+const getEpisodeCode = require("./get-episode-code");
+
+test("getEpisodeCode returns the episode code", () => {
+    expect(getEpisodeCode(3, 1)).toBe("S03E01");
+    expect(getEpisodeCode(3, 11)).toBe("S03E11");
+    expect(getEpisodeCode(10, 11)).toBe("S10E11");
+
+})
+
+export { }
