6aa3c503bf86ec459254e6c495093621bbf299c8
Hanna <sophianhanna@gmail.com>
2021-11-08 15:28:58 +0000

    Integrated getEpisodeCode

--- src/utils/get-episode-code.ts
+++ src/utils/get-episode-code.ts
@@ -1,10 +1,11 @@
-
 function getEpisodeCode(season: number, episode: number): string {
-    let str = [(season < 10) ? `S0${season}` : `S${season}`,
-    (episode < 10) ? `E0${episode}` : `E${episode}`];
-    return str.join("");
+  const str = [
+    season < 10 ? `S0${season}` : `S${season}`,
+    episode < 10 ? `E0${episode}` : `E${episode}`,
+  ];
+  return str.join("");
 }
 
-module.exports = getEpisodeCode;
-//export default getEpisodeCode;
-export { }
+//module.exports = getEpisodeCode;
+export default getEpisodeCode;
+//export { }
