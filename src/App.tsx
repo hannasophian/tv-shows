a3e64f2aeba324ca45ed23a66fc5b8b302f8f4b8
Hanna <sophianhanna@gmail.com>
2021-11-09 19:18:22 +0000

    Added search bar functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
 import episodes from "./episodes.json";
-import EpisodesEntry from "./EpisodesEntry";
+//import EpisodesEntry from "./EpisodesEntry";
 import AppFooter from "./AppFooter";
 import "./App.css";
 import SearchBar from "./SearchBar";
 
 function App(): JSX.Element {
-  const allEpisodes = episodes.map((episode) => (
-    <EpisodesEntry
-      key={episode.id}
-      id={episode.id}
-      url={episode.url}
-      name={episode.name}
-      season={episode.season}
-      number={episode.number}
-      type={episode.type}
-      airdate={episode.airdate}
-      airtime={episode.airtime}
-      airstamp={episode.airstamp}
-      runtime={episode.runtime}
-      image={{
-        medium: episode.image.medium,
-        original: episode.image.original,
-      }}
-      summary={episode.summary}
-      _links={{ self: { href: episode._links.self.href } }}
-    />
-  ));
   return (
     <div className="App">
       <h1>TV List</h1>
-      <SearchBar
-        displayedEpisodes={allEpisodes.length}
-        totalEpisodes={allEpisodes.length}
-      />
-      <div className="episodegrid">{allEpisodes}</div>
+      <SearchBar episodes={episodes} />
       <AppFooter />
     </div>
   );
--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,18 +1,59 @@
 import { useState } from "react";
+import IEpisode from "./utils/EpisodeType";
+import EpisodeList from "./EpisodeList";
 
 interface SearchBarProps {
-  displayedEpisodes: number;
-  totalEpisodes: number;
+  episodes: IEpisode[];
 }
 
 export default function SearchBar(props: SearchBarProps): JSX.Element {
-  const [inputTest, setInputText] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
+  const filteredEpisodes = findEpisodesMatching(query, props.episodes);
   return (
-    <div className="searchbar">
-      <input value={inputTest} onChange={(e) => setInputText(e.target.value)} />
-      <>
-        Displaying {props.displayedEpisodes}/{props.totalEpisodes} episodes
-      </>
-    </div>
+    <>
+      <div className="searchbar">
+        <input
+          id="searchInput"
+          className="control"
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search for keywords"
+        />
+        <>
+          <small>
+            Showing {filteredEpisodes.length}/{props.episodes.length} episodes
+          </small>
+        </>
+      </div>
+      <EpisodeList episodes={filteredEpisodes} />
+    </>
   );
+}
+
+function findEpisodesMatching(query: string, episodes: IEpisode[]) {
+  return episodes.filter((episode) => episodeMatchesQuery(episode, query));
+}
+
+//Return true if episode's name or summary
+//contains the given query string (case-insensitive)
+//An episode is ALWAYS considered to contain the empty string ""
+function episodeMatchesQuery(episode: IEpisode, query: string) {
+  return (
+    !query || contains(episode.name, query) || contains(episode.summary, query)
+  );
+}
+
+//Return true if string a contains string b, case-insensitive.  Else false.
+//Will return false if either a or b are falsy (e.g. empty strings)
+function contains(a: string, b: string) {
+  if (a === "" || b === "") {
+    return false;
+  } else {
+    return a.toLowerCase().includes(b.toLowerCase());
+  }
+  // return (
+  //     a && b &&
+  //     a.toLowerCase().includes(b.toLowerCase())
+  // );
 }
