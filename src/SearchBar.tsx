1f7658432935b801a84a37088e50d57719f85c7a
Hanna <sophianhanna@gmail.com>
2021-11-10 14:54:56 +0000

    Added drop down selector

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,59 +1,53 @@
 import { useState } from "react";
 import IEpisode from "./utils/EpisodeType";
 import EpisodeList from "./EpisodeList";
+import episodes from "./episodes.json";
+import findEpisodesMatching from "./utils/find-episodes-matching";
+import getEpisodeCode from "./utils/get-episode-code";
 
 interface SearchBarProps {
-  episodes: IEpisode[];
+    episodes: IEpisode[];
 }
 
 export default function SearchBar(props: SearchBarProps): JSX.Element {
-  const [query, setQuery] = useState<string>("");
-  const filteredEpisodes = findEpisodesMatching(query, props.episodes);
-  return (
-    <>
-      <div className="searchbar">
-        <input
-          id="searchInput"
-          className="control"
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for keywords"
-        />
+    const [query, setQuery] = useState<string>("");
+    //const [filteredEpisodes, setFilteredEpisodes] = useState(episodes);
+    const filteredEpisodes = findEpisodesMatching(query, props.episodes);
+    return (
         <>
-          <small>
-            Showing {filteredEpisodes.length}/{props.episodes.length} episodes
-          </small>
+            <div className="searchbar">
+                <select
+                    className="control"
+                    onChange={(event) => setQuery(event.target.value)} // = handleEpisodeSelected(event.target.value)}
+                    value={filteredEpisodes.length === 1 ? filteredEpisodes[0].id : ""}
+                >
+                    {
+                        //create the options within the select
+                        episodes.map((episode) => (
+                            <option key={episode.id} value={episode.id}>
+                                {getEpisodeCode(episode.season, episode.number)} -{" "}
+                                {episode.name}
+                                {/* {makeEpisodeCode(episode) + " - " + episode.name} */}
+                            </option>
+                        ))
+                    }
+                </select>
+                <input
+                    id="searchInput"
+                    className="control"
+                    type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Search for keywords"
+                />
+                <>
+                    <small>
+                        Showing {filteredEpisodes.length}/{props.episodes.length} episodes
+                    </small>
+                </>
+            </div>
+            <EpisodeList episodes={filteredEpisodes} />
         </>
-      </div>
-      <EpisodeList episodes={filteredEpisodes} />
-    </>
-  );
+    );
 }
 
-function findEpisodesMatching(query: string, episodes: IEpisode[]) {
-  return episodes.filter((episode) => episodeMatchesQuery(episode, query));
-}
-
-//Return true if episode's name or summary
-//contains the given query string (case-insensitive)
-//An episode is ALWAYS considered to contain the empty string ""
-function episodeMatchesQuery(episode: IEpisode, query: string) {
-  return (
-    !query || contains(episode.name, query) || contains(episode.summary, query)
-  );
-}
-
-//Return true if string a contains string b, case-insensitive.  Else false.
-//Will return false if either a or b are falsy (e.g. empty strings)
-function contains(a: string, b: string) {
-  if (a === "" || b === "") {
-    return false;
-  } else {
-    return a.toLowerCase().includes(b.toLowerCase());
-  }
-  // return (
-  //     a && b &&
-  //     a.toLowerCase().includes(b.toLowerCase())
-  // );
-}
